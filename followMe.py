f3bf6c45d9afdad7aa56eb53ecbe6069cc5668f5
toazbenj <90994176+toazbenj@users.noreply.github.com>
2021-12-09 11:44:08 -0500

    Uneccesary and Style
    You don't need these. Also made the code prettier.

--- followMe.py
+++ followMe.py
@@ -8,30 +8,30 @@ Follow Me Algorthm
         Register haptics
         Register Sensors
         Set time record keeping
-        
+
         Main Loop (60 second time sentinel)
-    
+
             Get teacher and student position data
             Calculate difference
             Display data
             Record data
             Select haptics index
-            
+
             If tolerance exceeded
                 Find direction of max difference
                 Modulate intensity
                 Play haptics
                 Sleep
                 Record intensity and direction
-                
+
         Close devices
         Close file
-        
+
     Except
         Close devices
         Close file
         Timestamp
-        
+
 Created on Thu Nov 18 11:42:07 2021
 @author: Ben Toaz
 """
@@ -44,127 +44,128 @@ from time import perf_counter
 # If any errors or crashes, sensors and files will still get closed
 try:
     # Open file for recoding data
-    file = open('sessionScratchWork.txt','w')
-    
+    file = open('sessionScratchWork.txt', 'w')
+
     # Dict of file names matched with keystrokes
     haptic_dict = {'a': "MoveLeft", 'd': 'MoveRight', 'w': "MoveForward",
                    's': 'MoveBack', 'q': 'TurnCCW', 'e': 'TurnCW', 'x': "Jump",
-                   'wa': 'ForwardLeft','wd': 'ForwardRight','sa': 'BackLeft',
+                   'wa': 'ForwardLeft', 'wd': 'ForwardRight', 'sa': 'BackLeft',
                    'sd': 'BackRight'}
-    
+
     # Numerical representation of direction for records
-    angle_dict = {'a': pi, 'd': 2*pi, 'w': pi/2,'s': 3*pi/2}
-    
+    angle_dict = {'a': pi, 'd': 2*pi, 'w': pi/2, 's': 3*pi/2}
+
     # Register haptic files
     sv.register(3)
-    
+
     # Register and Tare Sensors
     teacher, student, dong = sv.getDevices()
     teacher.tareWithCurrentOrientation()
     student.tareWithCurrentOrientation()
-    
+
     # Time recording values
     time = 0
     start = perf_counter()
     buzzes = 0
-    
+
     # Main Loop, 1 minute run time
     while time - start < 60:
-            
-            # Get batch of position data as (x,y,z) tuple, calculate difference
-            tec_tup = teacher.getStreamingBatch()
-            stu_tup = student.getStreamingBatch()
-            diff_tup = (stu_tup[0]-tec_tup[0],stu_tup[1]-tec_tup[1],
-                        stu_tup[2]-tec_tup[2])
-            
-            # Display and Record Data
-            print('Position Error {}, {}, {}'.format(
-                round(diff_tup[0],3),round(diff_tup[1],3),round(diff_tup[2],3)))
-            
-            # Timestamp, teacher, student, position error
-            file.write('\n'+str(time-start))
-            file.write('\n\n{},{},{}'.format(
-              round(tec_tup[0],3),round(tec_tup[1],3),round(tec_tup[2],3)))
-            file.write('\n{},{},{}'.format(
-              round(stu_tup[0],3),round(stu_tup[1],3),round(stu_tup[2],3)))
-            file.write('\n{},{},{}'.format(
-              round(diff_tup[0],3),round(diff_tup[1],3),round(diff_tup[2],3)))
-        
-            # Movement Cases
-            tolerance = pi/24
-            index = ''
-            
-            # # Mixed Conditions
-            # # fix ForwardLeft and ForwardRight, switched?
-            
-            # # Forward Left (-y difference and -z differnce)
-            # if diff_tup[1] <= -tolerance and diff_tup[2] <= -tolerance:
-            #     index = 'wa'
-                
-            # # Forward Right (-z differnce and +y difference)
-            # elif diff_tup[2] <= -tolerance and diff_tup[1] >= tolerance:
-            #     index = 'wd'
-                
-            # # Back Right (+y difference +z difference)
-            # elif diff_tup[1] >= tolerance and diff_tup[2] >= tolerance:
-            #     index = 'sd'
-    
-            # # Back Left (+z difference and -y difference)
-            # elif diff_tup[2] >= tolerance and diff_tup[1] <= -tolerance:
-            #     index = 'sa'
-            
-            # Pure Directions
-            # Left (-y difference)
-            if diff_tup[1] <= -tolerance:
-                index = 'a'
-                
-            # Forward (-z differnce)
-            elif diff_tup[2] <= -tolerance:
-                index = 'w'
-                
-            # Right (+y difference)
-            elif diff_tup[1] >= tolerance:
-                index = 'd'
-    
-            # Back (+z difference)
-            elif diff_tup[2] >= tolerance:
-                index = 's'
-            
-            # Play haptics
-            if index in haptic_dict:
-                # Decide which axis to check based on bigger difference
-                if abs(diff_tup[1]) > abs(diff_tup[2]):
-                    check_coord = 1
-                elif abs(diff_tup[1]) < abs(diff_tup[2]):
-                    check_coord = 2
-                
-                # Modulate intensity based on assumed max movement angle
-                intensity = abs(diff_tup[check_coord])/(pi/4)
-                # Can't exceed 1
-                if intensity > 1:
-                    intensity = 1
-                
-                sv.play(index=index, intensity=intensity, duration=0.5)
-                sleep(1.5)
-                buzzes += 1
-                
-                # Record intensity and direction (angle)
-                file.write('\n{},{}'.format(
-                    round(intensity,3),round(angle_dict[index],2)))
-            
-            else:
-                # No haptics => Intensity=0, Angle=0
-                file.write('\n{},{}'.format(0,0))
-            
-            # time = perf_counter()+0.1*buzzes
-            time = perf_counter()
-            
+
+        # Get batch of position data as (x,y,z) tuple, calculate difference
+        tec_tup = teacher.getStreamingBatch()
+        stu_tup = student.getStreamingBatch()
+        diff_tup = (stu_tup[0]-tec_tup[0], stu_tup[1]-tec_tup[1],
+                    stu_tup[2]-tec_tup[2])
+
+        # Display and Record Data
+        print('Position Error {}, {}, {}'.format(
+            round(
+                diff_tup[0], 3), round(diff_tup[1], 3), round(diff_tup[2], 3)))
+
+        # Timestamp, teacher, student, position error
+        file.write('\n'+str(time-start))
+        file.write('\n\n{},{},{}'.format(
+          round(tec_tup[0], 3), round(tec_tup[1], 3), round(tec_tup[2], 3)))
+        file.write('\n{},{},{}'.format(
+          round(stu_tup[0], 3), round(stu_tup[1], 3), round(stu_tup[2], 3)))
+        file.write('\n{},{},{}'.format(
+          round(diff_tup[0], 3), round(diff_tup[1], 3), round(diff_tup[2], 3)))
+
+        # Movement Cases
+        tolerance = pi/24
+        index = ''
+
+        # # Mixed Conditions
+        # # fix ForwardLeft and ForwardRight, switched?
+
+        # # Forward Left (-y difference and -z differnce)
+        # if diff_tup[1] <= -tolerance and diff_tup[2] <= -tolerance:
+        #     index = 'wa'
+
+        # # Forward Right (-z differnce and +y difference)
+        # elif diff_tup[2] <= -tolerance and diff_tup[1] >= tolerance:
+        #     index = 'wd'
+
+        # # Back Right (+y difference +z difference)
+        # elif diff_tup[1] >= tolerance and diff_tup[2] >= tolerance:
+        #     index = 'sd'
+
+        # # Back Left (+z difference and -y difference)
+        # elif diff_tup[2] >= tolerance and diff_tup[1] <= -tolerance:
+        #     index = 'sa'
+
+        # Pure Directions
+        # Left (-y difference)
+        if diff_tup[1] <= -tolerance:
+            index = 'a'
+
+        # Forward (-z differnce)
+        elif diff_tup[2] <= -tolerance:
+            index = 'w'
+
+        # Right (+y difference)
+        elif diff_tup[1] >= tolerance:
+            index = 'd'
+
+        # Back (+z difference)
+        elif diff_tup[2] >= tolerance:
+            index = 's'
+
+        # Play haptics
+        if index in haptic_dict:
+            # Decide which axis to check based on bigger difference
+            if abs(diff_tup[1]) > abs(diff_tup[2]):
+                check_coord = 1
+            elif abs(diff_tup[1]) < abs(diff_tup[2]):
+                check_coord = 2
+
+            # Modulate intensity based on assumed max movement angle
+            intensity = abs(diff_tup[check_coord])/(pi/4)
+            # Can't exceed 1
+            if intensity > 1:
+                intensity = 1
+
+            sv.play(index=index, intensity=intensity, duration=0.5)
+            bedtime = 1.5
+            sleep(bedtime)
+            buzzes += 1
+
+            # Record intensity and direction (angle)
+            file.write('\n{},{}'.format(
+                round(intensity, 3), round(angle_dict[index], 2)))
+
+        else:
+            # No haptics => Intensity=0, Angle=0
+            file.write('\n{},{}'.format(0, 0))
+
+        time = perf_counter()+bedtime*buzzes
+        time = perf_counter()
+
     sv.close([teacher, student, dong])
     file.close()
-    
+
 except:
     # Will execute no matter what
     sv.close([teacher, student, dong])
     file.close()
     file.write('\n'+str(time-start))
-    
--- sensorVestMethods.py
+++ sensorVestMethods.py
@@ -2,90 +2,90 @@
 """
 Sensor and Vest Methods
 
-
 Created on Thu Nov 18 10:58:51 2021
-
 @author: Ben Toaz
 """
 
 from bhaptics import better_haptic_player as player
 import threespace_api as ts_api
-import keyboard
 
 # Dict of file names matched with keystrokes
 haptic_dict = {'a': "MoveLeft", 'd': 'MoveRight', 'w': "MoveForward",
                's': 'MoveBack', 'q': 'TurnCCW', 'e': 'TurnCW', 'x': "Jump",
-               'wa': 'ForwardLeft','wd': 'ForwardRight','sa': 'BackLeft',
+               'wa': 'ForwardLeft', 'wd': 'ForwardRight', 'sa': 'BackLeft',
                'sd': 'BackRight'}
 
-#==============================================================================
 
 def getDevices():
-    ''' Search for docked devices, make list, assign names and orientation, return devices'''
-    
+    ''' Search for docked devices, make list, assign names and orientation,
+    return devices'''
+
     device_list = ts_api.getComPorts()
     com_port, friendly_name, device_type = device_list[0]
     dng_device = ts_api.TSDongle(com_port=com_port)
-    
-    device_dict = {1:dng_device[0],3:dng_device[1],4:dng_device[2]}
-    
+
+    device_dict = {1: dng_device[0], 3: dng_device[1], 4: dng_device[2]}
+
     key = input('Select teacher (1,3,4)>>')
     device1 = device_dict[int(key)]
-    
+
     key = input('Select student (1,3,4)>>')
     device2 = device_dict[int(key)]
-    
+
     device1.setStreamingSlots(slot0='getTaredOrientationAsEulerAngles')
     device2.setStreamingSlots(slot0='getTaredOrientationAsEulerAngles')
-    
+
     return device1, device2, dng_device
 
-#==============================================================================
 
 def register(iteration):
-    ''' Turn on haptic player, register all haptic files in dictionary 
+    ''' Turn on haptic player, register all haptic files in dictionary
     (letters = keys, names = values)'''
-    
+
     player.initialize()
     # Load Tact files from directory
     for value in haptic_dict.values():
         player.register(value+str(iteration), value+str(iteration)+".tact")
 
-#==============================================================================
 
 def play(index, intensity=1, duration=0.5, iteration=3):
     '''Takes keyboard input, intensity value; selects haptic file from dict;
     plays file with adjusted intensity, options for duration and rotation'''
-    
+
     # Find indicated motion
     if index in haptic_dict:
         print('\n'+haptic_dict[index])
-        
-        # Adjust haptics in real time
-        player.submit_registered_with_option(haptic_dict[index]+str(iteration), "alt",
-                                             scale_option={"intensity": intensity, "duration": duration},
-                                             rotation_option={"offsetAngleX": 0, "offsetY": 0})
 
-#==============================================================================
+        # Adjust haptics in real time
+        player.submit_registered_with_option(
+            haptic_dict[index]+str(iteration), "alt",
+            scale_option={"intensity": intensity, "duration": duration},
+            rotation_option={"offsetAngleX": 0, "offsetY": 0})
+
 
 def testPos(pos_tup1, pos_tup2, tolerance=0):
     '''Takes position tuple and margin of error tolerance, if position in each
     coordinate +/- toleance is less than 0.5 from 0, return true, else false'''
-    
+
+    # Currently not in use
+
     # Default values
     x_test_bool = False
     # y_test_bool = False
     z_test_bool = False
     decimals = 2
-    
+
     # Test for position close to origin within given tolerance
-    if (round(pos_tup1[0], decimals) >= (pos_tup2[0] - tolerance)) and (round(pos_tup1[0], decimals) <= (pos_tup2[0] + tolerance)):
+    if ((round(pos_tup1[0], decimals) >= (pos_tup2[0] - tolerance))
+        and (round(pos_tup1[0], decimals) <= (pos_tup2[0] + tolerance))):
         x_test_bool = True
-        
-    # if  round(pos_tup1[1], decimals) >= (pos_tup2[1] - tolerance) and round(pos_tup1[1], decimals) <= (pos_tup2[1] + tolerance):
+
+    # if (round(pos_tup1[1], decimals) >= (pos_tup2[1] - tolerance)
+        # and round(pos_tup1[1], decimals) <= (pos_tup2[1] + tolerance)):
     #     y_test_bool = True
-        
-    if  round(pos_tup1[2], decimals) >= (pos_tup2[2] - tolerance) and round(pos_tup1[2], decimals) <= (pos_tup2[2] + tolerance):
+
+    if (round(pos_tup1[2], decimals) >= (pos_tup2[2] - tolerance)
+        and round(pos_tup1[2], decimals) <= (pos_tup2[2] + tolerance)):
         z_test_bool = True
 
     # Test if all positions pass tests
@@ -95,10 +95,8 @@ def testPos(pos_tup1, pos_tup2, tolerance=0):
     else:
         return False
 
-#==============================================================================
 
 def close(device_lst):
     'Close all devices so next program can run'
     for d in device_lst:
         d.close()
-
