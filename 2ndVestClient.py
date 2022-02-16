7b99613845b4932aef5f5d576e793fbc9a241dc7
toazbenj <90994176+toazbenj@users.noreply.github.com>
2022-02-16 14:29:16 -0500

    Update 2ndVestClient.py

--- 2ndVestClient.py
+++ 2ndVestClient.py
@@ -5,7 +5,7 @@
     Initiazlize host and port
     Bind to socket
     Load tact files
-    
+
     Haptics loop
         Receive command
         Try
@@ -23,12 +23,12 @@ from bhaptics import better_haptic_player as player
 import utilitiesMethods as utility
 
 rvs_haptic_dict = {'d': "MoveLeft", 'a': 'MoveRight', 's': "MoveForward",
-                'w': 'MoveBack', 'sd': 'ForwardLeft', 'sa': 'ForwardRight', 
-                'wd': 'BackLeft','wa': 'BackRight'}
+                   'w': 'MoveBack', 'sd': 'ForwardLeft', 'sa': 'ForwardRight',
+                   'wd': 'BackLeft', 'wa': 'BackRight'}
 
 # Switches index key to new index value
-rvs_index_dict = {'d': "a", 'a': 'd', 's': "w", 'w': 's', 'sd': 'wa', 'sa': 'wd', 
-                'wd': 'sa','wa': 'sd'}
+rvs_index_dict = {'d': "a", 'a': 'd', 's': "w", 'w': 's', 'sd': 'wa',
+                  'sa': 'wd', 'wd': 'sa', 'wa': 'sd'}
 
 # Initialize host and port
 host = "35.12.209.242"
@@ -53,17 +53,17 @@ while(True):
     command = s.recv(1024)
     # Command is string of 1-2 letters and intensity float
     command = command.decode()
-    
+
     try:
         # Check if second character is an int, if so index is 1 letter long
         int(command[1])
         # Grab all intensity digits after index
         intensity = float(command[1:])
         index = command[0]
-    except ValueError:    
+
+    except ValueError:
         # Index is 2 letters long
         intensity = float(command[2:])
         index = command[0:2]
-    
+
     utility.play(index=rvs_index_dict[index], intensity=intensity)
-    
