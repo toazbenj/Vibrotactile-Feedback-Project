c01d1842824ad1c46a6f77906745a655f60434a9
toazbenj <90994176+toazbenj@users.noreply.github.com>
2022-03-02 14:40:03 -0500

    Update ballMovementTest.py

--- ballMovementTest.py
+++ ballMovementTest.py
@@ -9,14 +9,30 @@ Created on Tue Mar  1 14:24:14 2022
 import graphics
 import utilitiesMethods as utilities
 from math import pi
+from time import sleep
+import threespace_api as ts_api
 
 # Make Window
 x_bounds = 650
 y_bounds = 650
+max_movement_angle = pi/4
 window = graphics.GraphWin(width=x_bounds, height=y_bounds)
 
 # Register and Tare Sensors
-teacher, student, dongle, = utilities.getDevices()
+offset = 5
+
+device_list = ts_api.getComPorts()
+com_port, friendly_name, device_type = device_list[0]
+dng_device = ts_api.TSDongle(com_port=com_port)
+
+device_dict = {1: dng_device[0+offset], 3: dng_device[1+offset],
+               4: dng_device[2+offset]}
+
+key = input('Select device (1,3,4)>>')
+device1 = device_dict[int(key)]
+
+device1.setStreamingSlots(slot0='getTaredOrientationAsEulerAngles')
+device1.tareWithCurrentOrientation()
 
 # Sentinels/Conditions
 speed_limit = 15
@@ -32,35 +48,36 @@ ball.draw(window)
 while True:
 
     # Get position data
-    teacher_tup = teacher.getStreamingBatch()
+    teacher_tup = device1.getStreamingBatch()
    
     # Convert sensor angle movement to ball movement
-    x_move = teacher_tup[1] / (2*pi/4) * 10
-    y_move = teacher_tup[2] / (2*pi/4) * 10
+    x_pos = teacher_tup[1] / (max_movement_angle) * x_bounds
+    y_pos = teacher_tup[2] / (max_movement_angle) * y_bounds
 
-    # print('{},{}'.format(round(x_move,2),round(y_move,2)))
+    print('{},{}'.format(round(x_pos,2),round(y_pos,2)))
 
-    # If speed limit exceeded, sets speed to limit in same direction
-    if abs(x_move) > speed_limit:
-        x_move = speed_limit * (x_move/x_move)
-    if abs(y_move) > speed_limit:
-        y_move = speed_limit * (y_move/y_move)
+    # # If speed limit exceeded, sets speed to limit in same direction
+    # if abs(x_move) > speed_limit:
+    #     x_move = speed_limit * (x_move/x_move)
+    # if abs(y_move) > speed_limit:
+    #     y_move = speed_limit * (y_move/y_move)
 
     # Move ball, record motion within object
-    ball.move(-x_move, -y_move)
-    ball.x_center += x_move
-    ball.y_center += y_move
-
-    # Respawns ball in center of window if out of bounds
-    if ball.getCenter().x > x_bounds or ball.getCenter().y > y_bounds\
-            or ball.getCenter().x < 0 or ball.getCenter().y < 0:
-
-        point.undraw()
-        ball.undraw()
-
-        pt = graphics.Point(x_bounds/2, y_bounds/2)
-        ball = graphics.Circle(pt, 25)
-        ball.setOutline('blue')
-        ball.setFill('blue')
-        ball.draw(window)
+    # ball.move(-x_move, -y_move)
+    
+    if x_pos > x_bounds:
+        x_pos = x_bounds
+        
+    if x_pos < -x_bounds:
+       x_pos = -x_bounds
+       
+    if y_pos > y_bounds:
+        y_pos = y_bounds
+        
+    if y_pos < -y_bounds:
+       y_pos = -y_bounds
+       
+    point = graphics.Point(x_pos, y_pos)
+    ball = graphics.Circle(point, 25)
+    ball.draw(window)
    
