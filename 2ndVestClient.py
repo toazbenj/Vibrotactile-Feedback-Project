ececf8a5f5eaf39b6b689d30329704b23d796b35
toazbenj <90994176+toazbenj@users.noreply.github.com>
2022-04-19 15:26:58 -0400

    Update

--- 2ndVestClient.py
+++ 2ndVestClient.py
@@ -31,47 +31,49 @@ rvs_haptic_dict = {'d': "MoveLeft", 'a': 'MoveRight', 's': "MoveForward",
                    'w': 'MoveBack', 'sd': 'ForwardLeft', 'sa': 'ForwardRight',
                    'wd': 'BackLeft', 'wa': 'BackRight'}
 
-
-# Initialize host and port
-host = "35.12.209.98"
-
-port = 8080
-
-# Bind socket with port and host
-s = socket.socket()
-s.connect((host, port))
-print("Connected to Server.")
-
-# Picks Set of Haptic Feedback
-iteration = 4
-player.initialize()
-
-# Load Tact files from directory
-for value in rvs_haptic_dict.values():
-    player.register(value+str(iteration), value+str(iteration)+".tact")
-    
 try:
+    # Initialize host and port
+    host = "35.12.209.97"
     
-    # Haptics loop
-    while(True):
+    port = 8080
+    
+    # Bind socket with port and host
+    s = socket.socket()
+    s.connect((host, port))
+    print("Connected to Server.")
+    
+    # Picks Set of Haptic Feedback
+    iteration = 4
+    player.initialize()
+    
+    # Load Tact files from directory
+    for value in rvs_haptic_dict.values():
+        player.register(value+str(iteration), value+str(iteration)+".tact")
         
-        # Receive command from master program
-        command = s.recv(1024)
+    try:
         
-        # Command is string of 1-2 letters and intensity float
-        command = command.decode()
-        
-        # Recover index and intensity from command
-        command = command.split('-')
-        raw_intensity = float(command[0])
-        index = command[1]
-        teacher_intensity = float(command[2])
-        
-        # Play teacher haptics
-        utility.play(index=rvs_index_dict[index], 
-                     intensity=raw_intensity*teacher_intensity)
+        # Haptics loop
+        while(True):
+            
+            # Receive command from master program
+            command = s.recv(1024)
+            
+            # Command is string of 1-2 letters and intensity float
+            command = command.decode()
+            
+            # Recover index and intensity from command
+            command = command.split('-')
+            raw_intensity = float(command[0])
+            index = command[1]
+            teacher_intensity = float(command[2])
+            
+            # Play teacher haptics
+            utility.play(index=rvs_index_dict[index], 
+                         intensity=raw_intensity*teacher_intensity)
+    
+    # Server ends program
+    except ValueError:
+        print('Disconnected')
 
-# Server ends program
-except ValueError:
-    print('Disconnected')
-    
+except ConnectionRefusedError:
+    print("Check IP address")
